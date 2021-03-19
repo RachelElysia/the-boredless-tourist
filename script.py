0f53134917947cb5c6b64c8189630e6f332b7078
Rachel Elysia Perkins <rachel.elysia.perkins@gmail.com>
2021-03-19 08:35:17 +0000

    Add test objects

---
+++ script.py
@@ -0,0 +1,6 @@
+
+destinations = [“Paris, France”, “Shanghai, China”, “Los Angeles, USA”, “São Paulo, Brazil”, “Cairo, Egypt”]
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+def get_destination_index(destination)
